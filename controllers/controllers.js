e3b517df699dde4c224b1f8c8c7bc218c0d3b059
Bevard <sbevard1@gmail.com>
2017-05-22 20:07:22 -0400

    typo

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var models= require("../models");
+var models = require("../models");
 var express = require('express');
 var router = express.Router();
 // ===============================================================================
@@ -13,86 +13,86 @@ var team = "Clemson";
 //   ***The One Route to Route them all***
 // Route used to retrieve questions for any land
 router.get('/:land', function(req, res) {
-  models.Questions.findAll({
-  where: {
-    land: req.params.land
-    include: [{model: Questions}]
-  }
-}).then(function(data) {
-    console.log(data);
-    /*Render index.handlebars on root route*/
-    res.render("questions", {question:data});
-  });
+    models.Questions.findAll({
+        where: {
+            land: req.params.land
+        }
+    }).then(function(data) {
+        console.log(data);
+        /*Render index.handlebars on root route*/
+        res.render("questions", { question: data });
+    });
 });
 
 
 
 // To check if a usename is unique 
 router.get('/:username', function(req, res) {
-  models.Users.findOne({ where: {
-    userName:req.params.username
-  }}).then(function(data) {
+    models.Users.findOne({
+        where: {
+            userName: req.params.username
+        }
+    }).then(function(data) {
 
-    console.log(data);
+        console.log(data);
 
-  });
+    });
 });
 //To create an entry for a new user and log to console
 models.Users.create({
     userName: 'testUsername',
     teamName: 'teamName'
-  }).then(function(data) {
+}).then(function(data) {
     console.log(data);
-  });
+});
 
 //To select distinct team names
 models.Users.aggregate('teamName', 'DISTINCT', {
-  plain: false 
+    plain: false
 }).then(function(data) {
-   console.log(data);
-  });
+    console.log(data);
+});
 
 
 
 /*Get route for registration page*/
-  router.get('/registration', function(req, res) {
-  models.Users.aggregate('teamname', 'DISTINCT', { plain: false }).then(function(data) {
-    res.render("registration", {user:data});
-    console.log(data);
-  });
+router.get('/registration', function(req, res) {
+    models.Users.aggregate('teamname', 'DISTINCT', { plain: false }).then(function(data) {
+        res.render("registration", { user: data });
+        console.log(data);
+    });
 });
 // To check if a usename is unique
-models.Users.findAll({ where: {
-  userName:'arumita'
-  }}).then(function(data) {
-  console.log(data);
-  });
-
- /*Get route for standings page*/
-  router.get('/standings/', function(req, res) {
-  models.Users.findAll({
-  where: {
-  	teamName: team
-  	}
-  	}).then(function(data) {
-    //console.log(data);
-    
-    /*Render index.handlebars on root route*/
-    res.render("standings", {
-    userName:data,
-    team
-    });
-    });
-
-router.get('/', function(req, res) {
-  models.Questions.findAll().then(function(data) {
+models.Users.findAll({
+    where: {
+        userName: 'arumita'
+    }
+}).then(function(data) {
     console.log(data);
-    res.render("index");
-
-  });
 });
 
+/*Get route for standings page*/
+router.get('/standings/', function(req, res) {
+    models.Users.findAll({
+        where: {
+            teamName: team
+        }
+    }).then(function(data) {
+        //console.log(data);
 
+        /*Render index.handlebars on root route*/
+        res.render("standings", {
+            userName: data,
+            team
+        });
+    });
+});
 
+router.get('/', function(req, res) {
+    models.Questions.findAll().then(function(data) {
+        console.log(data);
+        res.render("index");
+    });
+});
 
 module.exports = router;
